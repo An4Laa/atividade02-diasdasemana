fd4b8a45c1eaf2ad3e3a50112574dda1b6d45b74
An4Laa <analaurarochadesouza6@gmail.com>
2023-10-09 14:53:35 -0300

    Atividade dias da semana

---
+++ diasdasemana.py
@@ -0,0 +1,9 @@
+dias_da_semana1 = "Segunda-feira"
+dias_da_semana2 = "Terça-feira"
+dias_da_semana3 = "Quarta-feira"
+dias_da_semana4 = "Quinta-feira"
+dias_da_semana5 = "Sexta-feira"
+dias_da_semana6 = "Sábado"
+dias_da_semana7 = "domingo"
+semana = [dias_da_semana1, dias_da_semana2, dias_da_semana3, dias_da_semana4, dias_da_semana5, dias_da_semana6, dias_da_semana7]
+print(semana)
